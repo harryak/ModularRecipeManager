1821d5a285da85a9be4cf6ee0fee40e41a167b6d
harryak <harryak@users.noreply.github.com>
2021-05-15 17:09:22 +0200

    Clean up project, add first model

---
+++ ModularRecipeManager/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModularRecipeManager.Models
+{
+    public class Recipe
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
